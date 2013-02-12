0c99d90a111a6afc4d1bd929878ab0a65a5a9c78
jeroendedauw <jeroendedauw@gmail.com>
2013-02-12 08:40:51 +0100

    started work on query object

---
+++ includes/language/Query.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Ask\Language;
+use Ask\Language\Description\Description;
+
+/**
+ * Object representing a query definition.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup Ask
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class Query implements \Ask\Immutable {
+
+	/**
+	 *
+	 *
+	 * @param Description $description
+	 * @param array $printRequests
+	 */
+	public function __construct( Description $description, array $printRequests = array() ) {
+
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @since 0.1
+	 *
+	 * @return integer
+	 */
+	public function getDepth() {
+		return 0;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @since 0.1
+	 *
+	 * @return boolean
+	 */
+	public function isSingleton() {
+		return false;
+	}
+
+}
